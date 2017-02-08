d5fb863645c72aa13630b37e78ba5a520f1d47e6
SeanWu2013 <sww35@scarletmail.rutgers.edu>
2017-02-07 22:45:01 -0500

    First step
    Most of this is copied and pasted from Sesh's Sakai project files; I started adding some stuff to read in a text file of songs and output them in an ObservableList (in view/ListController.java)

---
+++ SongLibrary/src/app/SLapp.java
@@ -0,0 +1,28 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SLapp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/f2c/view/F2C.fxml"));
+
+        GridPane root = (GridPane)loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Fahrenheit-Celsius");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
